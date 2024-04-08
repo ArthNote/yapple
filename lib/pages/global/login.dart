bea2bb4b666ac9acec5b62a8d026391e5e9a1798
Note <taehyungykim10@gmail.com>
2024-04-08 01:50:20 +0000

    test

--- lib/pages/global/login.dart
+++ lib/pages/global/login.dart
@@ -34,7 +34,7 @@ class LoginPage extends StatelessWidget {
                     height: 10,
                   ),
                   Text(
-                    "Welcome back! Please enter your details.",
+                    "hei tehre",
                     style: TextStyle(
                       color: Theme.of(context)
                           .colorScheme
