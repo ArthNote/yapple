601bda12435f268babf7f62a74aa9dfd6d112156
Note <taehyungykim10@gmail.com>
2024-04-08 15:45:42 +0000

    d

--- lib/pages/global/login.dart
+++ lib/pages/global/login.dart
@@ -21,108 +21,110 @@ class LoginPage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.background,
         body: SafeArea(
           child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(
-                        Icons.star_half_sharp,
-                        color: Theme.of(context).colorScheme.tertiary,
-                        size: 50,
-                      ),
-                      SizedBox(
-                        width: size.width * 0.68,
-                        child: Divider(
-                          height: 5,
-                          thickness: 1,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(
+                          Icons.star_half_sharp,
                           color: Theme.of(context).colorScheme.tertiary,
+                          size: 50,
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    "Log in to your account",
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.tertiary,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Welcome back! Please enter your details.",
-                    style: TextStyle(
-                      color: Theme.of(context)
-                          .colorScheme
-                          .tertiary
-                          .withOpacity(0.5),
-                      fontSize: 17,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  MyTextField(
-                    myController: emailController,
-                    isPass: false,
-                    hintText: "Enter your email",
-                    icon: Icons.email_outlined,
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  MyTextField(
-                    myController: passwordController,
-                    isPass: true,
-                    hintText: "Enter your password",
-                    icon: Icons.lock_outline,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        child: Text(
-                          "Forgot password",
-                          style: TextStyle(
+                        SizedBox(
+                          width: size.width * 0.68,
+                          child: Divider(
+                            height: 5,
+                            thickness: 1,
                             color: Theme.of(context).colorScheme.tertiary,
                           ),
                         ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Log in to your account",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.tertiary,
+                        fontSize: 32,
+                        fontWeight: FontWeight.w500,
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  MyButton(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    textColor: Theme.of(context).colorScheme.tertiary,
-                    label: "Log in",
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ChatsPage(),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Welcome back! Please enter your details.",
+                      style: TextStyle(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .tertiary
+                            .withOpacity(0.5),
+                        fontSize: 17,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    MyTextField(
+                      myController: emailController,
+                      isPass: false,
+                      hintText: "Enter your email",
+                      icon: Icons.email_outlined,
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    MyTextField(
+                      myController: passwordController,
+                      isPass: true,
+                      hintText: "Enter your password",
+                      icon: Icons.lock_outline,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          child: Text(
+                            "Forgot password",
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.tertiary,
+                            ),
+                          ),
                         ),
-                      );
-                    },
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                ],
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    MyButton(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      textColor: Theme.of(context).colorScheme.tertiary,
+                      label: "Log in",
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChatsPage(),
+                          ),
+                        );
+                      },
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
