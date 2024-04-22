6d4a469adab446658d3936d5d7d9e21a1439e255
Note <taehyungykim10@gmail.com>
2024-04-22 23:45:52 +0100

    home

--- lib/pages/students/home.dart
+++ lib/pages/students/home.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
-
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,7 +9,6 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Scaffold(
-
         backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
         body: SafeArea(child: Body()),
       ),
@@ -23,6 +21,79 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //news block
+          Container(
+            height: 170,
+            width: double.infinity,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Reminder Instanbul end of year trip",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "by: Academic Department",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.tertiary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(color: Colors.black12, blurRadius: 10),
+              ],
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+
+          //modules horizental list
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            "My Modules",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            height: 110,
+            width: 200,
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(color: Colors.black12, blurRadius: 10),
+              ],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Text("Mobile & Web Technologies"),
+          )
+        ],
+      ),
+    ));
   }
 }
