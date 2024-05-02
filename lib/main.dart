1e90d7d6b37be7baeeb14ae3c32f9b2a7dd9daf2
Note <taehyungykim10@gmail.com>
2024-05-02 18:36:21 +0100

    firebase packages + setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'pages/global/login.dart';
 
-void main() {
+void main() async {
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCgz5Tvjy5MITCuDMWzP9QckIT_CItW7XQ",
+      appId: "1:212222952999:android:a98dcdef7975a849353002",
+      messagingSenderId: "212222952999",
+      projectId: "yapple-1bc28",
+    ),
   );
   runApp(const MyApp());
 }
