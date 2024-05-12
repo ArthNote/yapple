36fbec6cffbd5028d76a72c15df292c3072d74de
fzpzz <githubfzz@gmail.com>
2024-05-12 21:15:01 +0000

    ZEDT CALENDAR WOOO

--- lib/pages/global/calendar.dart
+++ lib/pages/global/calendar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class calendarPage extends StatelessWidget {
   const calendarPage({super.key});
@@ -21,14 +22,40 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  CalendarFormat _calendarFormat = CalendarFormat.month; // Initialize calendar format
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('salam')
+        TableCalendar(
+          firstDay: DateTime.utc(2010, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: _focusedDay,
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          },
+          calendarFormat: _calendarFormat, // Set calendar format
+          onFormatChanged: (format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          },
+        ),
+        // Additional widgets can be added here if needed
       ],
     );
-    //nkhdm hna
   }
 }
+
+
+
 
